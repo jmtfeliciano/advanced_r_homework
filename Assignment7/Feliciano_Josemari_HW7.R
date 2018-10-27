a5a3524135e815f4c3322705b0dbedab66170945
Josemari Feliciano <josemari.feliciano@gmail.com>
2018-10-27 16:06:38 -0400

    Added labels and printed tables

--- Assignment7/Feliciano_Josemari_HW7.R
+++ Assignment7/Feliciano_Josemari_HW7.R
@@ -5,37 +5,51 @@
 wd<-"/Users/jfeliciano/Documents/advancedr/homework/Assignment7"
 setwd(wd) 
 
-# Installs and initializes dplyr library, it has select function that I will use later
-install.packages("dplyr") 
-library(dplyr)
-
 # loads csv file
 loaded_data <- read.csv("AdoptionData.csv", header=TRUE)
 
-# the list of variables copied from PDF HW6 prompt in one line
-desired_variables <- strsplit(split = ", " , x="uniqueid, COUNTRY, LIVING, ADOPTAGE, AGETST1, WTTST1, HTTST1, INDTST1, RESTST1, CXRDONE, GENDER" , ", ")[[1]]
+# vector with splitted list of variables requested by PDF HW6 prompt 
+desired_variables <- strsplit(split = ", " , x="uniqueid, COUNTRY, LIVING, ADOPTAGE, AGETST1, WTTST1, HTTST1, INDTST1, RESTST1, CXRDONE, GENDER" )[[1]]
 
 # subsets my data using select, selects data variables requested by hw prompt
-subset_data <- select(loaded_data, desired_variables)
+# note to self: the dplyr way is select(loaded_data, desired_variables)
+subset_data <- loaded_data[desired_variables]
 
 # filters data if uniqueid is non-empty
-filtered_data <- filter(subset_data, !is.na(uniqueid))
+# note to self:  the dplyr way is filter(subset_data, !is.na(uniqueid))
+analysis <- subset_data[!is.na(subset_data$uniqueid),]
 
-
-for(row in 1:nrow(filtered_data)) {
-  for(col in 1:ncol(filtered_data)) {
-    if(!is.na(filtered_data[row,col]) && filtered_data[row,col] == 99) {
-      filtered_data[row,col] <- NA
+# do nested for-loops to traverse all rows and columns, and replace 99 values to NA
+for(row in 1:nrow(analysis)) {
+  for(col in 1:ncol(analysis)) {
+    if(!is.na(analysis[row,col]) && analysis[row,col] == 99) {
+      analysis[row,col] <- NA
     }
   }
 }
 
-# note to self:  factors tell me the order is 0,1 and want to assign female to 0 and male to 1
+# creates levels and labels for the following varables:
+# COUNTRY, LIVING, ADOPTAGE, AGETST1, WTTST1, HTTST1, INDTST1, RESTST1, CXRDONE, GENDER
+analysis$COUNTRY <- factor(analysis$COUNTRY, levels = c(1:23),
+                                labels = c("China", "Russia", "Guatemala", "South Korea", "India", "Ukraine", "Thailand", 
+                                           "Ethiopia", "Phillipines", "Haiti", "Kenya", "Kazhakstan", "Singapore", "Bulgaria", 
+                                           "Romania", "Hawaii", "Belarus", "Cambodia", "Georgia", "Columbia", "Vietnam", 
+                                           "Siberia", "Moldavia"))
+analysis$LIVING <- factor(analysis$LIVING, levels = c(1:5), 
+                               labels = c("Orphanage only", "Foster care only", "Both orphanage and foster care", 
+                                          "Other", "Unknown"))
+analysis$RESTST1 <- factor(analysis$RESTST1, levels =c(1,2), labels = c("Positive","Negative"))
+analysis$CXRDONE <- factor(analysis$CXRDONE, levels = c(0,1,88), labels = c("No", "Yes", "CXR not required"))
+analysis$GENDER <- factor(analysis$GENDER, levels = c(0,1), labels = c("female","male"))
 
 
-attributes(filtered_data$GENDER)$labels<-c("Female", "Male")
 
+# prints tables from frequency for both gender and cxrdone
+table(analysis$GENDER)
+table(analysis$CXRDONE)
 
-hist(filtered_data$GENDER)
+# calculates BMI using WTTST1, HTTST1 variables, then calculates mean BMI
+analysis$BMI <- (analysis$WTTST1/(analysis$HTTST1**2))*10000
+mean(as.numeric(analysis$BMI), na.rm = TRUE)
 
                         
