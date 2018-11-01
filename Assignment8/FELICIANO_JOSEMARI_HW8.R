24824a1c85829c6ba948d408b106a6d2ba618ca7
Josemari Feliciano <josemari.feliciano@gmail.com>
2018-11-01 15:17:37 -0400

    Added preliminary files, initialized empty R file

---
+++ Assignment8/FELICIANO_JOSEMARI_HW8.R
@@ -0,0 +1,9 @@
+# Josemari Feliciano
+# HW 8
+
+wd<-"/Users/jfeliciano/Documents/advancedr/homework/Assignment8"
+setwd(wd) 
+
+demo_data <- read.csv("Demographics.csv", header = TRUE)
+test_original <- read.csv("TestScores_original.csv", header = TRUE)
+test_makeup <- read.csv("TestScores_makeup.csv", header = TRUE)
