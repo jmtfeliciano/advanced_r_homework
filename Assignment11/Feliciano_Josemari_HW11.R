0b3589cb1151d38bf6651f46c5f53e3ae6fc50bf
Josemari Feliciano <josemari.feliciano@gmail.com>
2018-12-02 14:27:00 -0500

    Wrote a script to simulate 10000 elections between Hillary and Trump

--- Assignment11/Feliciano_Josemari_HW11.R
+++ Assignment11/Feliciano_Josemari_HW11.R
@@ -8,4 +8,39 @@ setwd(wd)
 # loads election data from directory
 election_data <- read.csv("Election data.csv", header=TRUE)
 
+# changeable for later
+# number of simulation
+num_simulation <- 10000
+seed <- 12345
+
+
+# it loops through the states to determine win
+
+
+dem_all_ec <- c()
+rep_all_ec <- c()
+ind_all_ec <- c()
+
+for(sim in 1:num_simulation){
+  dem_ec_tally <- 0
+  rep_ec_tally <- 0
+  ind_ec_tally <- 0
+  
+  for(state in 1:nrow(election_data)) {
+    current_prob <- rdirichlet(1, c(election_data[state,2], election_data[state,3],  election_data[state,4]))
+    if(current_prob[1] > current_prob[2] && current_prob[1] > current_prob[3]) {
+      dem_ec_tally <- dem_ec_tally + election_data[state,"Electoral.Votes"]
+    }else if(current_prob[2] > current_prob[1] && current_prob[2] > current_prob[3]){
+      rep_ec_tally <- rep_ec_tally + election_data[state,"Electoral.Votes"]
+    }else{
+      ind_ec_tally <- ind_ec_tally + election_data[state,"Electoral.Votes"]
+    }
+  }
+  
+  dem_all_ec <- c(dem_all_ec, dem_ec_tally)
+  rep_all_ec <- c(rep_all_ec, rep_ec_tally)
+  ind_all_ec <- c(ind_all_ec, ind_ec_tally)
+}
+
+
 
