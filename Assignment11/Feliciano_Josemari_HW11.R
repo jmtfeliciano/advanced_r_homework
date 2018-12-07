f7544895a979a7ef2dff1e45777b29957ba4ccb1
Josemari Feliciano <josemari.feliciano@gmail.com>
2018-12-07 02:26:46 -0500

    Finalized documentation of my election_simulator() function

--- Assignment11/Feliciano_Josemari_HW11.R
+++ Assignment11/Feliciano_Josemari_HW11.R
@@ -3,6 +3,9 @@
 
 
 
+
+
+
 ######################################################################################################
 
 #Function: election_simulator()
@@ -32,9 +35,9 @@
 #           method = 4: assumes that all non-Democrats and non-Republicans voters would vote on 50%-50% split between Dems and Reps.  
 
 #Output: No direct return value. 
-#        It will print the following:
+#        It will print the following however:
 #           - on console, prints the probabilities of Democrats and Republicans outright winning, along with probability of undecided election
-#           - on plot tab, produces a single histogram (overlay) of the distribution of electoral votes for Republicans and Democrats 
+#           - creates a plot, produces a single histogram (overlay) of the distribution of electoral votes for Republicans and Democrats 
 
 #Example: election_simulator(file_location="/Users/jfeliciano/Election_Data.csv", seed_num=1234, num_sim=999, method=1)
 ########################################################################################################
@@ -47,17 +50,23 @@ election_simulator <- function(file_location, seed_num, num_sim, method){
   # reads the file
   election_data <- read.csv(file_location, header=TRUE)
   
-  # sets the seed
-  set.seed(seed_num) # assigns seed
+  #stops if the expected variables in the dataset do not exist or have length 0
+  if(length(election_data[,'Democrat_Per']) == 0 ||  length(election_data[,'Republican_Per']) == 0 || length(election_data[,'Other_Per']) == 0 || length(election_data[,'Electoral.Votes']) == 0) {
+    stop("Error: one (or more) of the required column variables is empty or undeclared; make sure to name your variables correctly as specified in documentation")
+  }
+  
+  # sets the seed that user has specified
+  set.seed(seed_num)
   
   # creates the tally of electoral college for each party for each simulation
   dem_ec_tally <- rep(0,num_sim)
   rep_ec_tally <- rep(0,num_sim)
   
-  # calculates the electoral college votes for each state in each simulation
+  # calculates the electoral college votes for each state in all simulation
   for(state in 1:nrow(election_data)) {
     
-    # determines the distribution of wins for each state for #num_sim simulations for all 4 different methods
+    # determines the distribution of wins for each state for num_sim simulations for all 4 different methods
+    # for binomial distribution, multinomial is utilized since binomial with two prob is just a subset of multinomial dist.
     if(method==1) { 
       current_state <- rmultinom(num_sim, 1, prob = c(election_data[state,'Democrat_Per'], election_data[state,'Republican_Per'],  election_data[state,'Other_Per']))
     }else if(method==2) {
@@ -70,9 +79,9 @@ election_simulator <- function(file_location, seed_num, num_sim, method){
     
     # adds the electoral votes to the vector of winner for current state for every single num_sim simulations
     for(col in 1:ncol(current_state)) {
-      if(current_state[1,col] == 1){
+      if(current_state[1,col] == 1) {
         dem_ec_tally[col] <- dem_ec_tally[col] + election_data[state,"Electoral.Votes"] 
-      }else if(current_state[2,col] == 1){
+      }else if(current_state[2,col] == 1) {
         rep_ec_tally[col] <- rep_ec_tally[col] + election_data[state,"Electoral.Votes"] 
       }
     }
@@ -81,28 +90,35 @@ election_simulator <- function(file_location, seed_num, num_sim, method){
   # once all calculations are done, creates a histogram with labels and a yellow line for 270 electors threshold
   hist(dem_ec_tally, col=rgb(0,0,1,0.2), xlim=c(0, 540), main=paste0("Simulated Electoral College Distribution\nUsing Method ",method," with ",num_sim, " simulations"), xlab="Electoral College Accrual")
   hist(rep_ec_tally, col=rgb(1,0,0,0.2), add=T)
-  legend("topright", c("Democratic", "Republican", "Overlap"), fill=c("Blue", "Red", "Purple"), cex=0.70)
+  legend("topright", c("Democratic", "Republican", "Overlap", "Required EC To Win"), fill=c("Blue", "Red", "Purple", "Yellow"), cex=0.60)
   box()
   abline(v = 270, lwd=4, col="yellow")
   
   # declares wins as 0, will be used to tabulate probabilities later
-  dem_win <- 0
+  dem_win <- 0 
   rep_win <- 0 
+  undecided <- 0
   
   # for each simulation, calculate who wins (whenever election is not undecided)
-  for(index in 1:length(dem_ec_tally)) {
+  for(index in 1:num_sim) {
     if(dem_ec_tally[index] > rep_ec_tally[index] && dem_ec_tally[index] >= 270) {
       dem_win <- dem_win + 1
     }else if(dem_ec_tally[index] < rep_ec_tally[index] && rep_ec_tally[index] >= 270) {
       rep_win <- rep_win + 1
+    }else{
+      undecided <- undecided + 1
     }
   }
   
-  # prints the probabilities for dem, rep, undecided (undecided is simply 1- p(dem) - p(rep))
-  print(paste0("Using Method ",method, ", the probabilities for the Democrats and Republicans to outright win are ",dem_win/num_sim, " and ",rep_win/num_sim,", respectively."," The probability of an undecided election is ",1-dem_win/num_sim-rep_win/num_sim))
+  # prints the probabilities for dem, rep, undecided
+  print(paste0("Using Method ",method, ", the probabilities for the Democrats and Republicans to outright win are ",dem_win/num_sim, " and ",rep_win/num_sim,", respectively."," The probability of an undecided election is ",undecided/num_sim))
   
 }
 
+
+
+
+#### FUNCTION CALLS FOR REQUESTED OUTPUT
 
 
 
@@ -114,10 +130,8 @@ setwd(wd)
 
 # all the function calls to perform 4 homework prompts
 # each function should take 10 seconds each
-
-election_simulator(file_location="Election data.csv", seed_num=1234, num_sim=10000, method=1)
-election_simulator(file_location="Election data.csv", seed_num=1234, num_sim=10000, method=2)
-election_simulator(file_location="Election data.csv", seed_num=1234, num_sim=10000, method=3)
-election_simulator(file_location="Election data.csv", seed_num=1234, num_sim=10000, method=4)
-
+election_simulator(file_location="Election data.csv", seed_num=12, num_sim=10000, method=1)
+election_simulator(file_location="Election data.csv", seed_num=12, num_sim=10000, method=2)
+election_simulator(file_location="Election data.csv", seed_num=12, num_sim=10000, method=3)
+election_simulator(file_location="Election data.csv", seed_num=12, num_sim=10000, method=4)
 
