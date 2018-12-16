f68b01644ad6af052cf765bc903d8acc356ab73d
Josemari Feliciano <josemari.feliciano@gmail.com>
2018-12-16 16:14:33 -0500

    Completed code and output for case when normal distribution is involved

--- RProject/FELICIANO_JOSEMARI_HW12.R
+++ RProject/FELICIANO_JOSEMARI_HW12.R
@@ -2,19 +2,59 @@
 # R-Project
 
 
-cluster_randomized_simulator <- function(seed,medical_practices,mean,num_sims,test,st_dev,min,max){
+cluster_randomized_simulator <- function(seed,medical_practices,mean,num_sims,dist,st_dev,min,max){
+  
+  # loads necessary stats package, install it if necessary
   if(!require("stats")) install.packages("stats") 
   library("stats") 
   set.seed(seed)
   
-  if(test==1){
-    
+  #initializes an empty dataframe with length reflecting necessary # of scenarios
+  if(dist==1) {
+    all_scenarios <- data.frame(scenario=numeric(length(st_dev)),
+                                average_cv=numeric(length(st_dev)),
+                                cutoff_90=numeric(length(st_dev)),
+                                acceptability=logical(length(st_dev)))
+  }else if(dist==2) {
+    all_scenarios <- data.frame(scenario=numeric(length(min)),
+                                average_cv=numeric(length(min)),
+                                cutoff_90=numeric(length(min)),
+                                acceptability=logical(length(min)))
+  }else{
+    stop("You are an idiot, please read my documentation.  Only enter 1 or 2, dammnit")
   }
   
+  # performs scenarios for the specified test
+  if(dist==1){
+    for(current_scenario in 1:length(st_dev)){ # start of current scenario loop
+      
+      cv_for_valid_sims <- c()
+      
+      while(length(cv_for_valid_sims) < num_sims) {
+        current_participants <- rnorm(medical_practices,mean,st_dev[current_scenario])
+        if(sum(current_participants) >= 6000 && sum(current_participants) <= 6100) {
+          current_cv <- sd(current_participants) / mean(current_participants)
+          cv_for_valid_sims <- c(cv_for_valid_sims, current_cv)
+        }
+      }
+      
+      all_scenarios[current_scenario,1] <- current_scenario
+      all_scenarios[current_scenario,2] <- mean(cv_for_valid_sims)
+      all_scenarios[current_scenario,3] <- quantile(cv_for_valid_sims, 0.90)
+      all_scenarios[current_scenario,4] <- 0.90 <= sum(cv_for_valid_sims < 0.23)/num_sims 
+      
+    } # end of current scenario loop
+    
+    
+  } # write else if for dist==2 later
+  
+ 
+  return(all_scenarios)  
+
 }
 
-cluster_randomized_simulator(seed=123, medical_practices=86, mean=70, num_sims=1000, test=1, st_dev=c(5,10,15,20))
-cluster_randomized_simulator(seed=123, medical_practices=86, mean=70, num_sims=1000, test=2, min=c(40,45,50), max=c(90,95,100))
+cluster_randomized_simulator(seed=123, medical_practices=86, mean=70, num_sims=1000, dist=1, st_dev=c(5,10,15,20))
+cluster_randomized_simulator(seed=123, medical_practices=86, mean=70, num_sims=1000, dist=2, min=c(40,45,50), max=c(90,95,100))
 
 
 
