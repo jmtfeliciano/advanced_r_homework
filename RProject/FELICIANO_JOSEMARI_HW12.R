f1aefc488190bd710d7c1633206f0f8ff07c4b04
Josemari Feliciano <josemari.feliciano@gmail.com>
2018-12-16 14:44:25 -0500

    Removed non-generalized solution, added backbone of function and function calls

--- RProject/FELICIANO_JOSEMARI_HW12.R
+++ RProject/FELICIANO_JOSEMARI_HW12.R
@@ -2,48 +2,6 @@
 # R-Project
 
 
-# current statistics
-medical_practices <- 86
-mean <- 70
-st_dev <- 5
-num_sims <- 1000
-
-# empty vector of valid simulations
-cv_for_valid_sims <- c()
-
-# runs until we have desired number of valid simulations
-while(length(cv_for_valid_sims) < num_sims) {
-  current_participants <- rnorm(medical_practices,mean,st_dev)
-  if(sum(current_participants) >= 6000 && sum(current_participants) <= 6100) {
-    current_cv <- sd(current_participants) / mean(current_participants)
-    cv_for_valid_sims <- c(cv_for_valid_sims, current_cv)
-  }
-}
-
-# need to check how many simulations have CV < 0.23
-acceptable <- sum(cv_for_valid_sims < 0.23)
-
-
-
-acceptable <- 0
-for(i in 1:1000){
-  if(cv_for_valid_sims[i] < 0.23){
-    acceptable <- acceptable + 1
-  }
-}
-
-
-if(acceptable/1000 > 0.90){
-  is
-}
-
-
-
-
-
-
-
-
 cluster_randomized_simulator <- function(seed,medical_practices,mean,num_sims,test,st_dev,min,max){
   if(!require("stats")) install.packages("stats") 
   library("stats") 
